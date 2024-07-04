5551e1a0cb6c0261e8ebaa0b37489e98cfb5de48
chandra shekhar tiwari <155417210+tiwarichandrashekhar@users.noreply.github.com>
2024-07-04 21:46:16 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class pattern
+{
+              public static void main(String args[])
+              {
+                 
+                  int n=5,i,k=0;
+                  i=n+1;
+                   k=k-n;
+ 
+ 
+                  while(i!=k)
+                  {
+                         System.out.print(Math.abs(k)+"  ");
+                          k++;
+                   }
+                            
+              }
+ 
+}
